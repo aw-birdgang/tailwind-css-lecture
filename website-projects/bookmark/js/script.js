391bfb9719634a892a10cb5fc9cbead5a9c4a286
birdgang <birdgang82@gmail.com>
2023-10-10 19:14:14 +0900

    add mobile menu

--- website-projects/bookmark/js/script.js
+++ website-projects/bookmark/js/script.js
@@ -1,16 +1,22 @@
 const tabs = document.querySelectorAll('.tab')
 const panels = document.querySelectorAll('.panel')
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+const logo = document.getElementById('logo')
 
 // Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+// Hamburger button listener
+btn.addEventListener('click', navToggle)
 
 function onTabClick(e) {
   // Deactivate all tabs
   tabs.forEach((tab) => {
     tab.children[0].classList.remove(
-      'border-softRed',
-      'border-b-4',
-      'md:border-b-0'
+        'border-softRed',
+        'border-b-4',
+        'md:border-b-0'
     )
   })
 
@@ -21,7 +27,19 @@ function onTabClick(e) {
   e.target.classList.add('border-softRed', 'border-b-4')
   const classString = e.target.getAttribute('data-target')
   document
-    .getElementById('panels')
-    .getElementsByClassName(classString)[0]
-    .classList.remove('hidden')
+      .getElementById('panels')
+      .getElementsByClassName(classString)[0]
+      .classList.remove('hidden')
+}
+
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+
+  if (menu.classList.contains('flex')) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg')
+  }
 }
